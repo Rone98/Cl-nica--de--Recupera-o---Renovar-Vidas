66ed2f287aef979fefb1c6f082606f6c3cd5d919
Rone98 <roneheliodev@gmail.com>
2026-03-19 16:54:06 +0100

    ajuste mobile sobre nos e slider

--- app.js
+++ app.js
@@ -15,3 +15,28 @@ function enviarMensagem(event) {
 
     window.open(url, '_blank')
 }
+
+const slides = document.querySelectorAll(".fotos div");
+
+let index = 0;
+
+setInterval(() => {
+
+
+    slides.forEach(slide => {
+        slide.classList.remove("item-ativo");
+        slide.classList.add("item");
+    });
+
+
+    index++;
+
+    if (index >= slides.length) {
+        index = 0;
+    }
+
+
+    slides[index].classList.remove("item");
+    slides[index].classList.add("item-ativo");
+
+}, 3000);
